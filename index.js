843314938c909d67703b30d3ce617c563e8fcb49
Alim501 <alimzhan.mamurbekov@mail.ru>
2026-02-01 16:52:30 +0500

    e+ fix

--- index.js
+++ index.js
@@ -21,20 +21,27 @@ function lcm(x, y) {
 }
 
 app.get("/app/alimzhan_mamurbekov_mail_ru", (req, res) => {
-  const xStr = req.query.x;
-  const yStr = req.query.y;
-
-  const isNatural = /^\d+$/;
-
-  if (!isNatural.test(xStr) || !isNatural.test(yStr)) {
-    res.send("NaN");
-    return;
-  }
-
-  const x = parseInt(xStr, 10);
-  const y = parseInt(yStr, 10);
-
-  if (x <= 0 || y <= 0) {
+const xStr = req.query.x?.replace(/ /g, '+'); 
+const yStr = req.query.y?.replace(/ /g, '+');
+  const x = Number(xStr);
+  const y = Number(yStr);
+  console.log(`Received x: ${xStr}, y: ${yStr}`);
+  console.log(`Parsed x: ${x}, y: ${y}`);
+  if (
+    isNaN(x) ||
+    isNaN(y) ||
+    !isFinite(x) ||
+    !isFinite(y) ||
+    !Number.isInteger(x) ||
+    !Number.isInteger(y) ||
+    x <= 0 ||
+    y <= 0
+  ) {
+    console.log("isNan", isNaN(x), isNaN(y));
+    console.log("isFinite", isFinite(x), isFinite(y));
+    console.log("isInteger", Number.isInteger(x), Number.isInteger(y));
+    console.log("x <= 0", x <= 0);
+    console.log("y <= 0", y <= 0);
     res.send("NaN");
     return;
   }
@@ -42,5 +49,4 @@ app.get("/app/alimzhan_mamurbekov_mail_ru", (req, res) => {
   const result = lcm(x, y);
   res.send(result.toString());
 });
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
